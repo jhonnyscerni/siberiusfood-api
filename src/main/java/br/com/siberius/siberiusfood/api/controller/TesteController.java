204b2a3576f0eaf6351a6e0b5d89a084693a49c3
jhonnyscerni <jhonnyscerni@gmail.com>
2020-01-26 21:23:51 -0300

    003.5 - Implementando um TesteController para testar End-Points

---
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/TesteController.java
@@ -0,0 +1,22 @@
+package br.com.siberius.siberiusfood.api.controller;
+
+import br.com.siberius.siberiusfood.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/teste")
+@RestController
+public class TesteController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping("/cozinhas/por-nome")
+    public ResponseEntity<?> cozinhasPorNome(@RequestParam("nome") String nome) {
+        return ResponseEntity.ok(cozinhaRepository.consultarPorNome(nome));
+    }
+}
