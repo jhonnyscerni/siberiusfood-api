7ddf6d73522b0d79eae483c7f53d116239a00ca4
jhonnyscerni <jhonnyscerni@gmail.com>
2020-01-30 21:35:58 -0300

    005.1 - Mapeamento muitos-para-muitos e Ajustes no RestauranteController

--- src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteController.java
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteController.java
@@ -57,7 +57,7 @@ public class RestauranteController {
             Restaurante restauranteAtual = restauranteRepository.findById(restauranteId).orElse(null);
 
             if (restauranteAtual != null) {
-                BeanUtils.copyProperties(restaurante, restauranteAtual, "id");
+                BeanUtils.copyProperties(restaurante, restauranteAtual, "id", "formaPagamentos");
                 restauranteAtual = restauranteService.salvar(restauranteAtual);
                 return ResponseEntity.ok(restauranteAtual);
             }
