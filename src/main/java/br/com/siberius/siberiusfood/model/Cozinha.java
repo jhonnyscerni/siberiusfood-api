54f037b2a88242bc36a30bdccd38a1503c23bd80
jhonnyscerni <jhonnyscerni@gmail.com>
2020-02-01 19:53:20 -0300

    005.3.1 - [Ajustes] Criando Relacionamentos (n-1)/(1-n)/(n/n)

--- src/main/java/br/com/siberius/siberiusfood/model/Cozinha.java
+++ src/main/java/br/com/siberius/siberiusfood/model/Cozinha.java
@@ -1,10 +1,13 @@
 package br.com.siberius.siberiusfood.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @JsonRootName("cozinha")
 @Data
@@ -21,5 +24,9 @@ public class Cozinha {
     //@JsonProperty("titulo")
     @Column(nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    private List<Restaurante> restaurantes = new ArrayList<>();
 
 }
