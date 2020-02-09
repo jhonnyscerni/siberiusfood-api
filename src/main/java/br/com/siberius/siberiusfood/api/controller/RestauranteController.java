277dd46e0c0fae4ce39aa4e5edd052b7af916c56
jhonnyscerni <jhonnyscerni@gmail.com>
2020-02-09 18:16:17 -0300

    007.2 - Ajustes no Metodo PATH

--- src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteController.java
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteController.java
@@ -5,14 +5,19 @@ import br.com.siberius.siberiusfood.exception.NegocioException;
 import br.com.siberius.siberiusfood.model.Restaurante;
 import br.com.siberius.siberiusfood.repository.RestauranteRepository;
 import br.com.siberius.siberiusfood.service.RestauranteService;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.lang.reflect.Field;
 import java.util.Map;
 
@@ -47,14 +52,15 @@ public class RestauranteController {
     }
 
     @PutMapping("/{restauranteId}")
-    public Restaurante atualizar(@PathVariable Long restauranteId, @RequestBody Restaurante restaurante) {
+    public Restaurante atualizar(@PathVariable Long restauranteId,
+                                 @RequestBody Restaurante restaurante) {
         try {
             Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
 
             BeanUtils.copyProperties(restaurante, restauranteAtual,
                     "id", "formaPagamentos", "endereco", "dataCadastro", "produtos");
-            return restauranteService.salvar(restauranteAtual);
 
+            return restauranteService.salvar(restauranteAtual);
         } catch (CozinhaNaoEncontradaException e) {
             throw new NegocioException(e.getMessage());
         }
@@ -62,40 +68,50 @@ public class RestauranteController {
 
     @PatchMapping("/{restauranteId}")
     public Restaurante atualizarParcial(@PathVariable Long restauranteId,
-                                        @RequestBody Map<String, Object> campos) {
+                                        @RequestBody Map<String, Object> campos, HttpServletRequest request) {
 
         Restaurante restauranteAtual = restauranteService.buscarOuFalhar(restauranteId);
 
-        merge(campos, restauranteAtual);
+        merge(campos, restauranteAtual, request);
 
         return atualizar(restauranteId, restauranteAtual);
     }
 
-    private void merge(@RequestBody Map<String, Object> dadosOrigem, Restaurante restauranteDestino) {
-        ObjectMapper objectMapper = new ObjectMapper();
+    private void merge(@RequestBody Map<String, Object> dadosOrigem, Restaurante restauranteDestino,
+                       HttpServletRequest request) {
+        ServletServerHttpRequest serverHttpRequest = new ServletServerHttpRequest(request);
         /**
          * Cria para mim uma instancia de Restaurante.class usando como base esse mapa de dados Origem para nao
          * da mais erro de conversao
          */
-        Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, true);
+            objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, true);
 
-        dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
-            /**
-             *   if (nomePropriedade.equals("nome")) {
-             *                 restauranteDestino.setNome((String) valorPropriedade);
-             *   }
-             */
+            Restaurante restauranteOrigem = objectMapper.convertValue(dadosOrigem, Restaurante.class);
 
-            Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
-            // Como campo e privado vamos da Acesso a ele
-            field.setAccessible(true);
+            dadosOrigem.forEach((nomePropriedade, valorPropriedade) -> {
+                /**
+                 *   if (nomePropriedade.equals("nome")) {
+                 *                 restauranteDestino.setNome((String) valorPropriedade);
+                 *   }
+                 */
 
-            // novoValor ja Convertido
-            Object novovalor = ReflectionUtils.getField(field, restauranteOrigem);
+                Field field = ReflectionUtils.findField(Restaurante.class, nomePropriedade);
+                // Como campo e privado vamos da Acesso a ele
+                field.setAccessible(true);
 
-            // System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novovalor);
+                // novoValor ja Convertido
+                Object novovalor = ReflectionUtils.getField(field, restauranteOrigem);
 
-            ReflectionUtils.setField(field, restauranteDestino, novovalor);
-        });
+                // System.out.println(nomePropriedade + " = " + valorPropriedade + " = " + novovalor);
+
+                ReflectionUtils.setField(field, restauranteDestino, novovalor);
+            });
+        } catch (IllegalArgumentException e) {
+            Throwable rootCause = ExceptionUtils.getRootCause(e);
+            throw new HttpMessageNotReadableException(e.getMessage(), rootCause, serverHttpRequest);
+        }
     }
 }
