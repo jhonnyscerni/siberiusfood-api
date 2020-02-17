4bc3f529a1e538bdb9834a27c3153b063b24047a
jhonnyscerni <jhonnyscerni@gmail.com>
2020-02-17 01:59:33 -0300

    009.4 - configurando base de teste e usando @TestPropertySource, Limpando a Base de Teste

--- src/test/java/br/com/siberius/siberiusfood/CadastroCozinhaIT.java
+++ src/test/java/br/com/siberius/siberiusfood/CadastroCozinhaIT.java
@@ -1,28 +1,44 @@
 package br.com.siberius.siberiusfood;
 
+import br.com.siberius.siberiusfood.model.Cozinha;
+import br.com.siberius.siberiusfood.repository.CozinhaRepository;
+import br.com.siberius.siberiusfood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource("/application-test.properties")
 public class CadastroCozinhaIT {
 
     @LocalServerPort
     private int port;
+
+    @Autowired
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
 
     @Before
     public void setUp(){
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/siberiusfood-api/cozinhas";
+
+        databaseCleaner.clearTables();
+        prepararDados();
     }
 
     @Test
@@ -45,9 +61,32 @@ public class CadastroCozinhaIT {
             .when()
                 .get()
             .then()
-                .body("", Matchers.hasSize(4))
+                .body("", Matchers.hasSize(2))
                 .body("nome", Matchers.hasItems("Indiana", "Tailandesa"));
 
+    }
+
+    @Test
+    public void deveRetornarStatus201_quandoCadastrarCozinha(){
+        RestAssured.given()
+                .body(" { \"nome\" : \"Chinesa \" }")
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+             .when()
+                .post()
+             .then()
+                .statusCode(HttpStatus.CREATED.value());
+
+    }
+
+    private void prepararDados() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Indiana");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha2);
     }
 
 }
