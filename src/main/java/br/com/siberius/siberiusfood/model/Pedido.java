f25d4a3f1a4e626f650c19e3acbea27fc0bc6ee7
jhonnyscerni <jhonnyscerni@gmail.com>
2020-03-14 19:33:39 -0300

    0011.19 - [IMPORTANTE] Refatorando o código de regras para transição de status de pedidos

--- src/main/java/br/com/siberius/siberiusfood/model/Pedido.java
+++ src/main/java/br/com/siberius/siberiusfood/model/Pedido.java
@@ -1,5 +1,6 @@
 package br.com.siberius.siberiusfood.model;
 
+import br.com.siberius.siberiusfood.exception.NegocioException;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -52,7 +53,7 @@ public class Pedido {
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
     private List<ItemPedido> itens = new ArrayList<>();
 
-    public void calcularValor(){
+    public void calcularValor() {
         this.subtotal = getItens().stream()
                 .map(item -> item.getPrecoTotal())
                 .reduce(BigDecimal.ZERO, BigDecimal::add);
@@ -70,12 +71,37 @@ public class Pedido {
         this.valorTotal = this.subtotal.add(this.taxaFrete);
     }
 
-    public void definirFrete(){
+    public void definirFrete() {
         setTaxaFrete(restaurante.getTaxaFrete());
     }
 
-    public void atribuirPedidoAosItens(){
+    public void atribuirPedidoAosItens() {
         getItens().forEach(item -> item.setPedido(this));
     }
 
+    public void confirmar() {
+        setStatus(StatusPedido.CONFIRMADO);
+        setDataConfirmacao(OffsetDateTime.now());
+    }
+
+    public void cancelar() {
+        setStatus(StatusPedido.CANCELADO);
+        setDataCancelamento(OffsetDateTime.now());
+    }
+
+    public void entragar() {
+        setStatus(StatusPedido.ENTREGUE);
+        setDataEntrega(OffsetDateTime.now());
+    }
+
+    private void setStatus(StatusPedido novoStatus) {
+        if (getStatus().naoPodeAlterarPara(novoStatus)) {
+            throw new NegocioException(
+                    String.format("Status do pedido %d não pode ser alterado de %s para %s",
+                            getId(), getStatus().getDescricao(),
+                            novoStatus.getDescricao()));
+        }
+
+        this.status = novoStatus;
+    }
 }
