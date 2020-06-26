b6c351515605dcceb6bfb12740d3a43df7c1c386
jhonnyscerni <jhonnyscerni@gmail.com>
2020-06-25 21:06:29 -0300

    0013.03 - Validando o content type do arquivo

--- src/main/java/br/com/siberius/siberiusfood/api/model/input/FotoProdutoInput.java
+++ src/main/java/br/com/siberius/siberiusfood/api/model/input/FotoProdutoInput.java
@@ -1,8 +1,10 @@
 package br.com.siberius.siberiusfood.api.model.input;
 
+import br.com.siberius.siberiusfood.core.validation.FileContentType;
 import br.com.siberius.siberiusfood.core.validation.FileSize;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Getter;
@@ -14,6 +16,7 @@ public class FotoProdutoInput {
 
     @NotNull
     @FileSize(max = "500KB")
+    @FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
     private MultipartFile arquivo;
 
     @NotBlank
