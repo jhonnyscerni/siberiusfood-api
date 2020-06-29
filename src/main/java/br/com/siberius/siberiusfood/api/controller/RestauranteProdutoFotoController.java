864f051abb3e6d9ed4f26c6ab947745b149aa170
jhonnyscerni <jhonnyscerni@gmail.com>
2020-06-29 20:24:04 -0300

    0013.09 - Servindo arquivos de fotos pela API - Exibindo a foto com Accept

--- src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteProdutoFotoController.java
@@ -3,14 +3,19 @@ package br.com.siberius.siberiusfood.api.controller;
 import br.com.siberius.siberiusfood.api.assembler.FotoProdutoDTOAssembler;
 import br.com.siberius.siberiusfood.api.model.FotoProdutoDTO;
 import br.com.siberius.siberiusfood.api.model.input.FotoProdutoInput;
+import br.com.siberius.siberiusfood.exception.EntidadeNaoEncontradaException;
 import br.com.siberius.siberiusfood.model.FotoProduto;
 import br.com.siberius.siberiusfood.model.Produto;
 import br.com.siberius.siberiusfood.service.CatalogoFotoProdutoService;
+import br.com.siberius.siberiusfood.service.FotoStorageService;
 import br.com.siberius.siberiusfood.service.ProdutoService;
 import java.io.IOException;
+import java.io.InputStream;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -30,6 +35,9 @@ public class RestauranteProdutoFotoController {
 
     @Autowired
     private FotoProdutoDTOAssembler assembler;
+
+    @Autowired
+    private FotoStorageService fotoStorageService;
 
     @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public FotoProdutoDTO atualizarFoto(@PathVariable Long restauranteId,
@@ -51,12 +59,27 @@ public class RestauranteProdutoFotoController {
 
     }
 
-    @GetMapping
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public FotoProdutoDTO buscar(@PathVariable Long restauranteId,
         @PathVariable Long produtoId) {
         FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
 
         return assembler.getCidadeDTO(fotoProduto);
+    }
+
+    @GetMapping(produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<InputStreamResource> servirFoto(@PathVariable Long restauranteId,
+        @PathVariable Long produtoId) {
+        try {
+            FotoProduto fotoProduto = catalogoFotoProdutoService.buscarOuFalhar(restauranteId, produtoId);
+            InputStream inputStream = fotoStorageService.recuperar(fotoProduto.getNomeArquivo());
+
+            return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(new InputStreamResource(inputStream));
+        } catch (EntidadeNaoEncontradaException e) {
+            return ResponseEntity.notFound().build();
+        }
     }
 
 }
