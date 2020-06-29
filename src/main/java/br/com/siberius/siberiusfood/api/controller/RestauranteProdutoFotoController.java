f226d7a666d1b011cd377f74320526920036f0e2
jhonnyscerni <jhonnyscerni@gmail.com>
2020-06-29 20:47:27 -0300

    0013.11 -  implementando endpoint de exclusão de foto de produto

--- src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteProdutoFotoController.java
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/RestauranteProdutoFotoController.java
@@ -15,14 +15,17 @@ import java.util.List;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -99,6 +102,13 @@ public class RestauranteProdutoFotoController {
         if (!compativel) {
             throw new HttpMediaTypeNotAcceptableException(mediaTypeAceitas);
         }
+    }
+
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void excluir(@PathVariable Long restauranteId,
+        @PathVariable Long produtoId) {
+        catalogoFotoProdutoService.excluir(restauranteId, produtoId);
     }
 
 }
--- src/main/java/br/com/siberius/siberiusfood/service/CatalogoFotoProdutoService.java
+++ src/main/java/br/com/siberius/siberiusfood/service/CatalogoFotoProdutoService.java
@@ -59,4 +59,14 @@ public class CatalogoFotoProdutoService {
         return produtoRepository.findFotoProdutoId(restauranteId, produtoId)
             .orElseThrow(() -> new FotoProdutoNaoEncontradaException(restauranteId, produtoId));
     }
+
+    @Transactional
+    public void excluir(Long restauranteId, Long produtoId) {
+        FotoProduto foto = buscarOuFalhar(restauranteId, produtoId);
+
+        produtoRepository.delete(foto);
+        produtoRepository.flush();
+
+        fotoStorageService.remover(foto.getNomeArquivo());
+    }
 }
