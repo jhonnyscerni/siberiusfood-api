968a03492b4690038b7b2874a8cc9be37caf98ab
jhonnyscerni <jhonnyscerni@gmail.com>
2020-06-30 17:22:58 -0300

    0013.15 - Implementando a exclusão de objetos do bucket da Amazon S3

--- src/main/java/br/com/siberius/siberiusfood/infrastructure/service/storage/S3FotoStorageService.java
+++ src/main/java/br/com/siberius/siberiusfood/infrastructure/service/storage/S3FotoStorageService.java
@@ -3,6 +3,7 @@ package br.com.siberius.siberiusfood.infrastructure.service.storage;
 import br.com.siberius.siberiusfood.core.storage.StorageProperties;
 import br.com.siberius.siberiusfood.service.FotoStorageService;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.DeleteObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import java.io.InputStream;
@@ -54,6 +55,16 @@ public class S3FotoStorageService implements FotoStorageService {
 
     @Override
     public void remover(String nomeArquivo) {
+        try {
+            String caminhoArquivo = getCaminhoArquivo(nomeArquivo);
+
+            DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(
+                storageProperties.getS3().getBucket(), caminhoArquivo);
+
+            amazonS3.deleteObject(deleteObjectRequest);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível excluir arquivo na Amazon S3.", e);
+        }
     }
 
 }
