6845e8e461d0f01cf8b461da8e8a5e15851aa39f
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-03 10:02:02 -0300

    0014.01- Configurando servidor SMTP e implementando serviço de estrutura de envio de email com spring

---
+++ src/main/java/br/com/siberius/siberiusfood/core/email/EmailProperties.java
@@ -0,0 +1,22 @@
+package br.com.siberius.siberiusfood.core.email;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Validated
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("siberius.email")
+public class EmailProperties {
+
+    @NotNull
+    private String remetente;
+
+}
