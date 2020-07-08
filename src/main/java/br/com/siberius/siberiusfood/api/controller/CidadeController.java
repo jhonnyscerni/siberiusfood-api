393b1d0b104035c5451e2a478f881ce4d065d7d0
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-08 15:15:34 -0300

    0017.03 - Descrevendo as operações de endpoints na documentação

--- src/main/java/br/com/siberius/siberiusfood/api/controller/CidadeController.java
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/CidadeController.java
@@ -9,16 +9,22 @@ import br.com.siberius.siberiusfood.exception.NegocioException;
 import br.com.siberius.siberiusfood.model.Cidade;
 import br.com.siberius.siberiusfood.repository.CidadeRepository;
 import br.com.siberius.siberiusfood.service.CidadeService;
-import org.springframework.beans.BeanUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import java.util.List;
+import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.List;
-
-import io.swagger.annotations.Api;
 @Api(tags = "Cidades")
 @RestController
 @RequestMapping(value = "/cidades")
@@ -36,16 +42,19 @@ public class CidadeController {
     @Autowired
     private CidadeInputDTODisassembler disassembler;
 
+    @ApiOperation("Lista as cidades")
     @GetMapping
     public List<CidadeDTO> listar() {
         return assembler.getListCidadeDTO(cidadeRepository.findAll());
     }
 
+    @ApiOperation("Busca uma cidade por ID")
     @GetMapping("/{cidadeId}")
     public CidadeDTO buscar(@PathVariable Long cidadeId) {
         return assembler.getCidadeDTO(cidadeService.buscarOuFalhar(cidadeId));
     }
 
+    @ApiOperation("Cadastra uma cidade")
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public CidadeDTO salvar(@RequestBody @Valid CidadeInputDTO cidadeInputDTO) {
@@ -58,6 +67,7 @@ public class CidadeController {
         }
     }
 
+    @ApiOperation("Atualiza uma cidade por ID")
     @PutMapping("/{cidadeId}")
     public CidadeDTO atualizar(@RequestBody @Valid CidadeInputDTO cidadeInputDTO, @PathVariable Long cidadeId) {
         try {
@@ -72,6 +82,7 @@ public class CidadeController {
         }
     }
 
+    @ApiOperation("Exclui uma cidade por ID")
     @DeleteMapping("/{cidadeId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void remover(@PathVariable Long cidadeId) {
