e72e015d6b7cd187d4569cf20f8a36397b633da5
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-12 15:20:56 -0300

    0017.29 - descrevendo documentação de endpoints de usuários

--- src/main/java/br/com/siberius/siberiusfood/api/model/input/UsuarioComSenhaDTO.java
+++ src/main/java/br/com/siberius/siberiusfood/api/model/input/UsuarioComSenhaDTO.java
@@ -1,5 +1,6 @@
 package br.com.siberius.siberiusfood.api.model.input;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +8,6 @@ import lombok.Setter;
 @Setter
 public class UsuarioComSenhaDTO extends UsuarioInputDTO {
 
+    @ApiModelProperty(example = "123", required = true)
     private String senha;
 }
