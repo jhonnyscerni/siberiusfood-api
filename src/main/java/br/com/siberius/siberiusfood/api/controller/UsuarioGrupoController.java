df30b1a64a7111b7664dadcbc39735a2940e8710
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-12 15:24:31 -0300

    0017.30 - descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/br/com/siberius/siberiusfood/api/controller/UsuarioGrupoController.java
+++ src/main/java/br/com/siberius/siberiusfood/api/controller/UsuarioGrupoController.java
@@ -2,17 +2,19 @@ package br.com.siberius.siberiusfood.api.controller;
 
 import br.com.siberius.siberiusfood.api.assembler.GrupoDTOAssembler;
 import br.com.siberius.siberiusfood.api.model.GrupoDTO;
+import br.com.siberius.siberiusfood.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import br.com.siberius.siberiusfood.model.Usuario;
 import br.com.siberius.siberiusfood.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(value = "usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private UsuarioService usuarioService;
@@ -29,14 +31,14 @@ public class UsuarioGrupoController {
     @PutMapping("/{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void associar(@PathVariable Long usuarioId,
-                         @PathVariable Long grupoId) {
+        @PathVariable Long grupoId) {
         usuarioService.associarGrupo(usuarioId, grupoId);
     }
 
     @DeleteMapping("/{grupoId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void desassociar(@PathVariable Long usuarioId,
-                            @PathVariable Long grupoId) {
+        @PathVariable Long grupoId) {
         usuarioService.desassociarGrupo(usuarioId, grupoId);
     }
 }
