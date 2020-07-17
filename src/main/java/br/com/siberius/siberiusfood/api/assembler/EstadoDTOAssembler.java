aad05a6051b1a07a5c1badf53dbab47a0ed3e2f5
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-16 21:42:26 -0300

    0018.06 - adicionando hypermedia nos recursos de estados

--- src/main/java/br/com/siberius/siberiusfood/api/assembler/EstadoDTOAssembler.java
+++ src/main/java/br/com/siberius/siberiusfood/api/assembler/EstadoDTOAssembler.java
@@ -1,9 +1,13 @@
 package br.com.siberius.siberiusfood.api.assembler;
 
+import br.com.siberius.siberiusfood.api.controller.EstadoController;
 import br.com.siberius.siberiusfood.api.model.EstadoDTO;
 import br.com.siberius.siberiusfood.model.Estado;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.Collections;
@@ -11,18 +15,36 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class EstadoDTOAssembler {
+public class EstadoDTOAssembler extends RepresentationModelAssemblerSupport<Estado, EstadoDTO> {
 
     @Autowired
     private ModelMapper modelMapper;
 
-    public EstadoDTO getEstadoDTO(Estado estado){
-        return modelMapper.map(estado, EstadoDTO.class);
+    public EstadoDTOAssembler() {
+        super(EstadoController.class, EstadoDTO.class);
     }
 
-    public List<EstadoDTO> getListEstadoDTO(List<Estado> estados){
-        return estados.stream()
-                .map(estado -> getEstadoDTO(estado))
-                .collect(Collectors.toList());
+    @Override
+    public EstadoDTO toModel(Estado estado) {
+        EstadoDTO estad = createModelWithId(estado.getId(), estado);
+        modelMapper.map(estado, estad);
+
+        estad.add(WebMvcLinkBuilder.linkTo(EstadoController.class).withRel("estados"));
+
+        return estad;
+//        return modelMapper.map(estado, EstadoDTO.class);
     }
+
+    @Override
+    public CollectionModel<EstadoDTO> toCollectionModel(Iterable<? extends Estado> entities) {
+        return super.toCollectionModel(entities)
+            .add(WebMvcLinkBuilder.linkTo(EstadoController.class).withSelfRel());
+    }
+
+//    public List<EstadoDTO> getListEstadoDTO(List<Estado> estados) {
+//        return estados.stream()
+//            .map(estado -> getEstadoDTO(estado))
+//            .collect(Collectors.toList());
+//    }
+
 }
