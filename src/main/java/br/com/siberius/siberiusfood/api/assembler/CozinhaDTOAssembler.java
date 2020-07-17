cf3c2d381b9ac1d91f46cbd4f36dbb0c1d32a30c
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-17 19:04:35 -0300

    0018.07 -  Adicionando hypermedia em recursos com paginação

--- src/main/java/br/com/siberius/siberiusfood/api/assembler/CozinhaDTOAssembler.java
+++ src/main/java/br/com/siberius/siberiusfood/api/assembler/CozinhaDTOAssembler.java
@@ -1,27 +1,44 @@
 package br.com.siberius.siberiusfood.api.assembler;
 
+import br.com.siberius.siberiusfood.api.controller.CozinhaController;
 import br.com.siberius.siberiusfood.api.model.CozinhaDTO;
 import br.com.siberius.siberiusfood.model.Cozinha;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
-public class CozinhaDTOAssembler {
+public class CozinhaDTOAssembler extends RepresentationModelAssemblerSupport<Cozinha, CozinhaDTO> {
 
     @Autowired
     private ModelMapper modelMapper;
 
-    public CozinhaDTO getCozinhaDTO(Cozinha cozinha){
-       return modelMapper.map(cozinha, CozinhaDTO.class);
+    public CozinhaDTOAssembler() {
+        super(CozinhaController.class, CozinhaDTO.class);
     }
 
-    public List<CozinhaDTO> getLisCozinhasDTO(List<Cozinha> cozinhas){
-        return cozinhas.stream()
-                .map(cozinha -> getCozinhaDTO(cozinha))
-                .collect(Collectors.toList());
+    @Override
+    public CozinhaDTO toModel(Cozinha cozinha) {
+        CozinhaDTO cozinhaDTO = createModelWithId(cozinha.getId(), cozinha);
+        modelMapper.map(cozinha, cozinhaDTO);
+
+        cozinhaDTO.add(WebMvcLinkBuilder.linkTo(CozinhaController.class).withRel("cozinhas"));
+
+        return cozinhaDTO;
+
+        //return modelMapper.map(cozinha, CozinhaDTO.class);
     }
+
+//    public List<CozinhaDTO> getLisCozinhasDTO(List<Cozinha> cozinhas) {
+//        return cozinhas.stream()
+//            .map(cozinha -> getCozinhaDTO(cozinha))
+//            .collect(Collectors.toList());
+//    }
+
+
 }
--- src/main/java/br/com/siberius/siberiusfood/api/openapi/controller/CozinhaControllerOpenApi.java
+++ src/main/java/br/com/siberius/siberiusfood/api/openapi/controller/CozinhaControllerOpenApi.java
@@ -10,12 +10,13 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedModel;
 
 @Api(tags = "Cozinhas")
 public interface CozinhaControllerOpenApi {
 
     @ApiOperation("Lista as cozinhas com paginação")
-    Page<CozinhaDTO> listar(Pageable pageable);
+    PagedModel<CozinhaDTO> listar(Pageable pageable);
 
     @ApiOperation("Busca uma cozinha por ID")
     @ApiResponses({
