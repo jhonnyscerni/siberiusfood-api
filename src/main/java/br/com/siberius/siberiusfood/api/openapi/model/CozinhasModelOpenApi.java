c06e2cc1aaf2940df2a259c51fc35c64cdbb6b30
jhonnyscerni <jhonnyscerni@gmail.com>
2020-07-21 20:32:52 -0300

    0018.25 - Corrigindo a paginação na documentação

--- src/main/java/br/com/siberius/siberiusfood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/br/com/siberius/siberiusfood/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,10 +1,29 @@
 package br.com.siberius.siberiusfood.api.openapi.model;
 
 
+import br.com.siberius.siberiusfood.api.model.CidadeDTO;
 import br.com.siberius.siberiusfood.api.model.CozinhaDTO;
+import br.com.siberius.siberiusfood.api.openapi.model.CidadeCollectionModelOpenApi.CidadeEmbeddedModelOpenApi;
 import io.swagger.annotations.ApiModel;
+import java.util.List;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.hateoas.Links;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaDTO> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
 
+    private CozinhasEmbeddedModelOpenApi _embedded;
+    private Links _links;
+
+    @ApiModel("CozinhasEmbeddedModel")
+    @Data
+    public class CozinhasEmbeddedModelOpenApi {
+
+        private List<CozinhaDTO> cozinhas;
+
+    }
 }
